3a52cacaed9a2018c9c720d0424d34a829ca3651
Mohin Mokashi <mokashimohin7@gmail.com>
2026-08-03 15:03:56 +0530

    Add API Gateway and register with Eureka

---
+++ backend/api-gateway/src/test/java/com/smart_habit_tracker/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_habit_tracker.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
